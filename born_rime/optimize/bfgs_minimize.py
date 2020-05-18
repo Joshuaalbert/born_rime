a7ecc98b651e59d3b886a98204846c5a4ca094d2
Joshua Albert <albert@strw.leidenuniv.nl>
2020-05-19 00:52:46 +0200

    * JAX implementation of BFGS and strong Wolfe inexact line search

--- born_rime/optimize/bfgs_minimize.py
+++ born_rime/optimize/bfgs_minimize.py
@@ -3,38 +3,56 @@ https://pages.mtu.edu/~struther/Courses/OLD/Sp2013/5630/Jorge_Nocedal_Numerical_
 """
 
 import jax
-import jax.numpy as np
+import jax.numpy as jnp
 from jax.lax import while_loop
 from .line_search import line_search
-import collections
+from typing import NamedTuple, Optional
 
-BFGSResults = collections.namedtuple(
+class OptimizeResults(NamedTuple):
+    x: jnp.ndarray
+    success : bool
+    status : int
+    message : str
+    fun : jnp.ndarray
+    jac : jnp.ndarray
+    hess_inv : jnp.ndarray
+    nfev : int
+    njev : int
+    nit : int
+
+BFGSResults = NamedTuple(
     'BFGSResults', [
-        'converged',  # bool, True if minimization converges
-        'failed',  # bool, True if line search fails
-        'k',  # The number of iterations of the BFGS update.
-        'nfev',  # The total number of objective evaluations performed.
-        'ngev',  # total number of jacobian evaluations
-        'nhev',  # total number of hessian evaluations
-        'x_k',  # A tensor containing the last argument value found during the search. If the search converged, then
+        ('converged', bool),  # bool, True if minimization converges
+        ('failed', bool),  # bool, True if line search fails
+        ('k', int),  # The number of iterations of the BFGS update.
+        ('nfev', int),  # The total number of objective evaluations performed.
+        ('ngev', int),  # total number of jacobian evaluations
+        ('nhev', int),  # total number of hessian evaluations
+        ('x_k', jnp.ndarray),
+        # A tensor containing the last argument value found during the search. If the search converged, then
         # this value is the argmin of the objective function.
-        'f_k',  # A tensor containing the value of the objective
+        ('f_k', jnp.ndarray),  # A tensor containing the value of the objective
         # function at the `position`. If the search
         # converged, then this is the (local) minimum of
         # the objective function.
-        'g_k',  # A tensor containing the gradient of the
+        ('g_k', jnp.ndarray),  # A tensor containing the gradient of the
         # objective function at the
         # `final_position`. If the search converged
         # the max-norm of this tensor should be
         # below the tolerance.
-        'H_k'  # A tensor containing the inverse of the estimated Hessian.
+        ('H_k', jnp.ndarray)  # A tensor containing the inverse of the estimated Hessian.
     ])
+
 
 
 def bfgs_minimize(func, x0, options=None):
     """
-        Minimisation with BFGS. Inexact line search is performed satisfying both Wolfe conditions
-        (strong version of second one).
+        The BFGS algorithm from
+        Algorithm 6.1 from Wright and Nocedal, 'Numerical Optimization', 1999, pg. 136-143
+
+        Notes:
+            We utilise boolean arithmetic to avoid jax.cond calls which don't work on accelerators.
+
         Args:
             func: function to minimise, should take a single flat parameter, similar to numpy
             x0: initial guess of parameter
@@ -44,8 +62,9 @@ def bfgs_minimize(func, x0, options=None):
                     method each iteration may use several function evaluations.
                 analytic_initial_hessian: bool,
                     whether to use JAX to get hessian for the initialisation, otherwise it's eye.
+                    **experimental**
                 g_tol: float
-                    stopping criteria is |grad(func)|_2 \lt g_tol
+                    stopping criteria is |grad(func)|_2 lt g_tol
                 ls_maxiter: int or None which means inf
                     Maximum number of line search iterations to perform
 
@@ -54,10 +73,10 @@ def bfgs_minimize(func, x0, options=None):
     """
     if options is None:
         options = dict()
-    maxiter: int = options.get('maxiter', None)
-    analytic_initial_hessian: bool = options.get('analytic_initial_hessian', True)
-    g_tol: float = options.get('g_tol', 1e-8)
-    ls_maxiter: int = options.get('ls_maxiter', 50)
+    maxiter: Optional[int] = options.get('maxiter', None)
+    analytic_initial_hessian: bool = options.get('analytic_initial_hessian', False)
+    g_tol: float = options.get('g_tol', 1e-5)
+    ls_maxiter: int = options.get('ls_maxiter', 10)
 
     state = BFGSResults(converged=False,
                         failed=False,
@@ -71,26 +90,29 @@ def bfgs_minimize(func, x0, options=None):
                         H_k=None)
 
     if maxiter is None:
-        maxiter = np.inf
+        maxiter = jnp.inf
 
     D = x0.shape[0]
 
     if analytic_initial_hessian:
         hess = jax.hessian(func, argnums=0)
         initial_B = hess(x0)
-        initial_H = np.linalg.pinv(initial_B)
+        initial_H = jnp.linalg.pinv(initial_B)
+        if jnp.any(jnp.linalg.eigvals(initial_B)<0):
+            initial_H = jnp.eye(D)
         state = state._replace(nhev=state.nhev + 1)
+
     else:
-        initial_H = np.eye(D)
+        initial_H = jnp.eye(D)
 
     value_and_grad = jax.value_and_grad(func)
 
     f_0, g_0 = value_and_grad(x0)
     state = state._replace(f_k=f_0, g_k=g_0, H_k=initial_H, nfev=state.nfev + 1, ngev=state.ngev + 1,
-                           converged=np.linalg.norm(g_0) < g_tol)
+                           converged=jnp.linalg.norm(g_0) < g_tol)
 
     def body(state):
-        p_k = -np.dot(state.H_k, state.g_k)
+        p_k = -jnp.dot(state.H_k, state.g_k)
         line_search_results = line_search(value_and_grad, state.x_k, p_k, f_0=state.f_k, g_0=state.g_k,
                                           max_iterations=ls_maxiter)
         state = state._replace(nfev=state.nfev + line_search_results.nfev,
@@ -101,13 +123,13 @@ def bfgs_minimize(func, x0, options=None):
         f_kp1 = line_search_results.f_k
         g_kp1 = line_search_results.g_k
         y_k = g_kp1 - state.g_k
-        rho_k = np.reciprocal(np.dot(y_k, s_k))
+        rho_k = jnp.reciprocal(jnp.dot(y_k, s_k))
 
         sy_k = s_k[:, None] * y_k[None, :]
-        w = np.eye(D) - rho_k * sy_k
-        H_kp1 = np.dot(np.dot(w, state.H_k), w.T) + rho_k * s_k[:, None] * s_k[None, :]
+        w = jnp.eye(D) - rho_k * sy_k
+        H_kp1 = jnp.dot(jnp.dot(w, state.H_k), w.T) + rho_k * s_k[:, None] * s_k[None, :]
 
-        converged = np.linalg.norm(g_kp1) < g_tol
+        converged = jnp.linalg.norm(g_kp1) < g_tol
 
         state = state._replace(converged=converged,
                                k=state.k + 1,
@@ -124,6 +146,7 @@ def bfgs_minimize(func, x0, options=None):
         body,
         state)
 
+    # while (~ state.converged) & (~state.failed) & (state.k < maxiter):
+    #     state = body(state)
+
     return state
-
-
--- born_rime/optimize/minimize.py
+++ born_rime/optimize/minimize.py
@@ -1,6 +1,5 @@
 from .bfgs_minimize import bfgs_minimize
 
-
 def minimize(fun, x0, *, method=None, tol=None, options=None):
     """
     Interface to scalar function minimisation.
@@ -22,47 +21,3 @@ def minimize(fun, x0, *, method=None, tol=None, options=None):
     if method.lower() == 'bfgs':
         return bfgs_minimize(fun, x0, options=options)
     raise ValueError("Method {} not recognised".format(method))
-
-
-def test_minimize():
-
-    def rosenbrock(x):
-        return np.sum(100. * np.diff(x) ** 2 + (1. - x[:-1]) ** 2)
-
-
-    x0 = np.zeros(2)
-
-
-    @jax.jit
-    def min_op(x0):
-        result = minimize(rosenbrock, x0, analytic_initial_hessian=True)
-        return result
-
-    jax_res1 = min_op(x0)
-
-    print("Final with analytic initialisation:\n", jax_res1 )
-
-    jax_res1_nojit = minimize_nojit(rosenbrock, x0, analytic_initial_hessian=True)
-
-    assert np.all(jax_res1.x_k == jax_res1_nojit.x_k)
-
-    @jax.jit
-    def min_op(x0):
-        result = minimize(rosenbrock, x0, analytic_initial_hessian=False)
-        return result
-
-    jax_res2 = min_op(x0)
-
-    print("Final eye initialisation (like scipy):\n", jax_res2)
-
-    from scipy.optimize import minimize as smin
-    import numpy as onp
-
-    def rosenbrock(x):
-        return onp.sum(100. * onp.diff(x) ** 2 + (1. - x[:-1]) ** 2)
-
-    scipy_res = smin(rosenbrock, x0, method='BFGS')
-    print("Scipy:\n", scipy_res)
-
-    assert np.all(np.isclose(scipy_res.x, jax_res1.x_k))
-    assert np.all(np.isclose(scipy_res.x, jax_res2.x_k))
