4f8f7d0b16574b819791a39ea80459cbd8ebd012
Joshua Albert <albert@strw.leidenuniv.nl>
2020-05-21 02:20:04 +0200

    * numpyro req for testing ideas * some formatting

--- setup.py
+++ setup.py
@@ -6,20 +6,19 @@ from setuptools import find_packages
 __minimum_numpy_version__ = '1.16.2'
 __minimum_jax_version__ = '0.1.67'
 
-setup_requires = ['numpy>=' + __minimum_numpy_version__, 
-'jax>='+__minimum_jax_version__]
+setup_requires = ['numpy>=' + __minimum_numpy_version__,
+                  'jax>=' + __minimum_jax_version__]
 
 setup(name='born_rime',
       version='0.0.1',
       description='Born approximation to the RIME',
       author=['Joshua G. Albert'],
       author_email=['albert@strw.leidenuniv.nl'],
-    setup_requires=setup_requires,  
-    tests_require=[
-        'pytest>=2.8',
-    ],
-    # package_data= {'born_rime':['arrays/*', 'data/*']},
-   package_dir = {'':'./'},
-   packages=find_packages('./')
-     )
-
+      setup_requires=setup_requires,
+      tests_require=[
+          'pytest>=2.8',
+      ],
+      # package_data= {'born_rime':['arrays/*', 'data/*']},
+      package_dir={'': './'},
+      packages=find_packages('./')
+      )
